969c2bcc44f8f09e5bf6f5083d344b518379a0df
peerfahad <fahadrashid611@gmail.com>
2021-11-25 19:00:04 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,22 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { GameComponent } from './game/game.component';
 import { HomeComponent } from './home/home.component';
-import { WinComponent } from './win/win.component';
+import { HttpClientModule } from '@angular/common/http'; 
+
 
 @NgModule({
   declarations: [
     AppComponent,
     GameComponent,
-    HomeComponent,
-    WinComponent
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
