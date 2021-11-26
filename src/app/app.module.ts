ddb7515c389788bc909a849868256944be9caeca
peerfahad <fahadrashid611@gmail.com>
2021-11-26 15:34:39 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { GameComponent } from './game/game.component';
 import { HomeComponent } from './home/home.component';
-import { HttpClientModule } from '@angular/common/http'; 
+
 
 
 @NgModule({
